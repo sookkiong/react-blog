aea969eaf89347ed665b66ca6bb5a47e1ba5b39f
sookkiong <solnaamu98@gmail.com>
2022-07-08 15:44:35 +0900

    fix: state 단순화

--- src/App.js
+++ src/App.js
@@ -5,12 +5,8 @@ import { useState } from 'react';
 function App() {
 
   let post= '강남 우동 맛집';
-  let [womanCoat, setWomanCoat] = useState('여자 코트 추천');
-  let [dining, setDining] = useState('강남 우동 맛집');
-  let [study, setStudy] = useState('파이썬 독학');
+  let [title, setTitle] = useState( ['남자코트 추천', '강남 우동 맛집', '파이썬 독학'] );
 
-
-  
   
   return (
     <div className="App">
@@ -19,17 +15,17 @@ function App() {
       </div>
 
       <div className="list">
-        <h4>{ womanCoat }</h4>
+        <h4>{ title[0] }</h4>
         <p>2월 17일 발행</p>
       </div>
 
       <div className="list">
-        <h4>{ dining }</h4>
+        <h4>{ title[1] }</h4>
         <p>2월 17일 발행</p>
       </div>
 
       <div className="list">
-        <h4>{ study }</h4>
+        <h4>{ title[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
 
