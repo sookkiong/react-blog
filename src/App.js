06e3f6a4c9dba425fb4f21412320229224921361
sookkiong <solnaamu98@gmail.com>
2022-07-08 15:33:38 +0900

    feat: useState 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post= '강남 우동 맛집';
+  let [womanCoat, setWomanCoat] = useState('여자 코트 추천');
+  let [dining, setDining] = useState('강남 우동 맛집');
+  let [study, setStudy] = useState('파이썬 독학');
 
+
+  
+  
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={ {color:'red', fontSize:'20px'} }>블로그임</h4>
+        <h4 style={ {color:'red', fontSize:'20px'} }>ReactBlog</h4>
       </div>
 
       <div className="list">
-        <h4>글제목</h4>
+        <h4>{ womanCoat }</h4>
         <p>2월 17일 발행</p>
       </div>
 
-      <h4>{ post }</h4>
+      <div className="list">
+        <h4>{ dining }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>{ study }</h4>
+        <p>2월 17일 발행</p>
+      </div>
 
     </div>
   );
+  
 }
 
 export default App;
