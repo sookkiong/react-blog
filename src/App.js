39033edb9816003eb89ade0bbde87d1f0b67ef95
sookkiong <solnaamu98@gmail.com>
2022-07-12 15:27:33 +0900

    feat: title 오름차순 정렬 구현

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   let post= '강남 우동 맛집';
   let [title, setTitle] = useState( ['남자코트 추천', '강남 우동 맛집', '파이썬 독학'] );
   let [따봉, 따봉변경] = useState(0);
-  let [woman, setWoman] = useState('남자 코트 추천');
 
   
   return (
@@ -17,10 +16,23 @@ function App() {
         <h4 style={ {color:'red', fontSize:'20px'} }>ReactBlog</h4>
       </div>
 
-      <div className='change'><span onClick={ () => { setWoman('여자 코트 추천') } }>여자로 바꿈</span></div>
+      <button onClick={ () => {
+        // arr에 title 배열을 받아와서 sort()로 가나다순 정렬을 한 뒤 state변경을 함
+        let arr = [...title];
+        arr.sort();
+        setTitle(arr);
+
+      } }>가나다순 정렬</button>
+
+      <button onClick={ () => {
+        let copy = [...title];
+        copy[0] = '여자 코트 추천';
+        setTitle(copy);
+
+      } }>성별변경</button>
       
       <div className="list">
-        <h4>{ woman } <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> {따봉} </h4>
+        <h4>{ title[0] } <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
 
